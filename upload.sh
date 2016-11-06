3966a463eed8c51dbed3af55128b211f1d21f2d2
probonopd <probonopd@users.noreply.github.com>
2016-11-06 15:41:27 +0100

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -13,7 +13,7 @@ if [ ! -z "$TRAVIS_REPO_SLUG" ] ; then
   REPO_SLUG="$TRAVIS_REPO_SLUG"
   if [ -z "$GITHUB_TOKEN" ] ; then
     echo "$GITHUB_TOKEN missing, please set it in the Travis CI settings of this project"
-    echo "You can get one from https://github.com/settings/applications"
+    echo "You can get one from https://github.com/settings/tokens"
     exit 1
   fi
 else
@@ -21,16 +21,25 @@ else
   echo "Not running on Travis CI, this is currently not supported"
   REPO_SLUG="probonopd/uploadtool"
   if [ -z "$GITHUB_TOKEN" ] ; then
-    read -s -p "Token (https://github.com/settings/applications): " GITHUB_TOKEN
+    read -s -p "Token (https://github.com/settings/tokens): " GITHUB_TOKEN
   fi
 fi
 
-release_url="https://api.github.com/repos/$REPO_SLUG/releases/$RELEASE_NAME"
-delete_url="https://api.github.com/repos/$REPO_SLUG/releases"
+delete_url="https://api.github.com/repos/$REPO_SLUG/git/refs/tags/$RELEASE_NAME"
+
+#delete_url=$(curl -GET --silent \
+#    --header "Authorization: token ${GITHUB_TOKEN}" \
+#    "${tag_url}" | grep '"url":' | head -n 1 | cut -d '"' -f 4 )
+
+echo "delete_url: $delete_url"
 
 curl -XDELETE --silent \
     --header "Authorization: token ${GITHUB_TOKEN}" \
-    "${release_url}"
+    "${delete_url}"
+
+release_url="https://api.github.com/repos/$REPO_SLUG/releases/$RELEASE_NAME"
+curl -H "Authorization: token ${GITHUB_TOKEN}" \
+     --data '{"tag_name": "'"$RELEASE_NAME"'","target_commitish": "master","name": "'"$RELEASE_NAME"'","body": "'"$RELEASE_NAME"'","draft": false,"prerelease": true}' "https://api.github.com/repos/$REPO_SLUG/releases"
 
 curl -H "Authorization: token ${GITHUB_TOKEN}" \
      -H "Accept: application/vnd.github.manifold-preview" \
