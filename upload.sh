probonopd/uploadtool

commit 23a86b72c44d8d3a405dd3e9d378e9bf2d3fb475

Add more information to release details

upload.sh
@@ -85,7 +85,13 @@ if [ "$TRAVIS_COMMIT" != "$tag_sha" ] ; then
   fi
 
   if [ ! -z "$TRAVIS_JOB_ID" ] ; then
-    BODY="Travis CI build log: https://travis-ci.org/$REPO_SLUG/builds/$TRAVIS_BUILD_ID/"
+    read -r -d '' BODY <<EOF
+Release generated by Travis CI build no. $TRAVIS_BUILD_NUMBER.
+
+Build log: https://travis-ci.org/$REPO_SLUG/builds/$TRAVIS_BUILD_ID/
+
+_Generated by [uploadtool](https://github.com/probonopd/uploadtool)._
+EOF
   else
     BODY=""
   fi

commit 2878bf62286299e0a40ac434ad7515f68b0396ed

Quickfix for bug introduced in 23a86b7

upload.sh
@@ -2,6 +2,27 @@
 
 set +x # Do not leak information
 
+rawurlencode() {
+  local string="$1"
+  local strlen=${#string}
+  local encoded=""
+  local pos c o
+
+  for (( pos=0 ; pos<strlen ; pos++ )); do
+     c=${string:$pos:1}
+     case "$c" in
+        [-_.~a-zA-Z0-9])
+            o="$c"
+            ;;
+        * )
+            printf -v o '%%%02x' "'$c"
+            ;;
+     esac
+     encoded+="$o"
+  done
+  echo "$encoded"
+}
+
 RELEASE_NAME="continuous" # Do not use "latest" as it is reserved by GitHub
 
 if [ "$TRAVIS_EVENT_TYPE" == "pull_request" ] ; then
@@ -91,7 +112,7 @@ if [ "$TRAVIS_COMMIT" != "$tag_sha" ] ; then
   fi
 
   release_infos=$(curl -H "Authorization: token ${GITHUB_TOKEN}" \
-       --data '{"tag_name": "'"$RELEASE_NAME"'","target_commitish": "'"$TRAVIS_BRANCH"'","name": "'"Continuous build"'","body": "'"$BODY"'","draft": false,"prerelease": true}' "https://api.github.com/repos/$REPO_SLUG/releases")
+       --data '{"tag_name": "'"$RELEASE_NAME"'","target_commitish": "'"$TRAVIS_BRANCH"'","name": "'"Continuous build"'","body": "'$(rawurlencode "$BODY")'","draft": false,"prerelease": true}' "https://api.github.com/repos/$REPO_SLUG/releases")
 
   echo "$release_infos"
 